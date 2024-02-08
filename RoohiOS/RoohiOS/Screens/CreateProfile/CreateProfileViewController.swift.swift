af62abd8fa1c398046b5f640803199637aebc81f
d.uladzislau <u.dzmitryieu@andersenlab.com>
2024-02-08 19:44:03 +0300

    've created iOS app + structured

---
+++ RoohiOS/RoohiOS/Screens/CreateProfile/CreateProfileViewController.swift.swift
@@ -0,0 +1,19 @@
+//
+//  ProfileViewController.swift
+//  RoohiOS
+//
+//  Created by Cezar_ on 08.02.24.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
